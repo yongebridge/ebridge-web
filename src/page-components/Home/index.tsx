2a87c77049c42db9d695cc4579fe61f7b8a5eb1b
ebridge-lxy <seya@ebridge.exchange>
2023-07-27 16:30:09 +0800

    feat: add notification and configurable mask for home page

--- src/page-components/Home/index.tsx
+++ src/page-components/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FromCard, ToCard } from './Card';
 import HomeProvider from './HomeContext';
 import SelectTokenModal from './SelectTokenModal';
@@ -11,24 +11,49 @@ import History from './History';
 import useMediaQueries from 'hooks/useMediaQueries';
 import { useLanguage } from 'i18n';
 import PageHead from 'components/PageHead';
+import { Notification, NotificationForPhone } from 'components/Notification';
+import Mask from './Mask';
+// import { baseRequest } from 'api';
 export default function Home() {
   const isMd = useMediaQueries('md');
+  const flag = false;
   const { t } = useLanguage();
+
+  console.log('xxxxx');
+
+  useEffect(() => {
+    async function fetchData() {
+      // await baseRequest({
+      //   url: `http://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/page/initData`,
+      //   // params: { address },
+      //   method: 'GET',
+      // });
+      await fetch(`https://3xsw4ap8wah59.cfc-execute.bj.baidubce.com/api/amis-mock/mock2/page/initData`);
+    }
+    fetchData();
+  }, []);
   return (
     <HomeProvider>
       <PageHead title={t('Token Bridge')} />
-      <div className={styles.body}>
-        {isMd && <h2 className={styles.title}>{t('Token Bridge')}</h2>}
-        <FromCard />
-        <ChangeIcon />
-        <ToCard />
-        <Notice />
-        {!isMd && <ActionButton />}
-        <History />
-      </div>
-      {isMd && <ActionButton />}
-      <SelectTokenModal />
-      <AddTokenModal />
+      {flag ? (
+        <>
+          <div className={styles.body}>
+            {isMd && <h2 className={styles.title}>{t('Token Bridge')}</h2>}
+            <FromCard />
+            <ChangeIcon />
+            <ToCard />
+            <Notice />
+            {!isMd && <ActionButton />}
+            <History />
+          </div>
+          {isMd && <ActionButton />}
+          <SelectTokenModal />
+          <AddTokenModal />
+          {isMd ? <NotificationForPhone /> : <Notification />}
+        </>
+      ) : (
+        <Mask />
+      )}
     </HomeProvider>
   );
 }
