380f1879c191706a247b173ca925f92a270e94fb
ebridge-lxy <seya@ebridge.exchange>
2023-07-28 15:23:53 +0800

    feat: update notificatiion and replace text

--- src/page-components/Home/index.tsx
+++ src/page-components/Home/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { FromCard, ToCard } from './Card';
 import HomeProvider from './HomeContext';
 import SelectTokenModal from './SelectTokenModal';
@@ -11,24 +10,41 @@ import History from './History';
 import useMediaQueries from 'hooks/useMediaQueries';
 import { useLanguage } from 'i18n';
 import PageHead from 'components/PageHead';
+import { Notification, NotificationForPhone } from 'components/Notification';
+import useMaskQuery from 'hooks/useMaskQuery';
+import Mask from './Mask';
+import { Skeleton } from 'antd';
+
 export default function Home() {
   const isMd = useMediaQueries('md');
   const { t } = useLanguage();
+  const { isShowMask, isLoading } = useMaskQuery();
+  if (isLoading) {
+    return <Skeleton paragraph={{ rows: 10 }} />;
+  }
+
   return (
     <HomeProvider>
       <PageHead title={t('Token Bridge')} />
-      <div className={styles.body}>
-        {isMd && <h2 className={styles.title}>{t('Token Bridge')}</h2>}
-        <FromCard />
-        <ChangeIcon />
-        <ToCard />
-        <Notice />
-        {!isMd && <ActionButton />}
-        <History />
-      </div>
-      {isMd && <ActionButton />}
-      <SelectTokenModal />
-      <AddTokenModal />
+      {!isShowMask ? (
+        <>
+          <div className={styles.body}>
+            {isMd && <h2 className={styles.title}>{t('Token Bridge')}</h2>}
+            <FromCard />
+            <ChangeIcon />
+            <ToCard />
+            <Notice />
+            {!isMd && <ActionButton />}
+            <History />
+          </div>
+          {isMd && <ActionButton />}
+          <SelectTokenModal />
+          <AddTokenModal />
+          {isMd ? <NotificationForPhone /> : <Notification />}
+        </>
+      ) : (
+        <Mask />
+      )}
     </HomeProvider>
   );
 }
