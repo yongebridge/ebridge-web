8fdde69ef73f36df1a2a6edb35b9ad4a48721fab
ebridge-lxy <seya@ebridge.exchange>
2023-08-08 14:07:02 +0800

    feat: update contract address for mainnet

--- src/constants/platform/tDVV.ts
+++ src/constants/platform/tDVV.ts
@@ -7,7 +7,7 @@ export const CHAIN_INFO = {
 
 export const TOKEN_CONTRACT = '7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX';
 export const CROSS_CHAIN_CONTRACT = '2snHc8AMh9QMbCAa7XXmdZZVM5EBZUUPDdLjemwUJkBnL6k8z9';
-export const BRIDGE_CONTRACT = '8XmxcaQGCRzrfP4ij3C6SR4nDbHkYBnKJxcAoWPtFofvtmahL';
+export const BRIDGE_CONTRACT = 'GZs6wyPDfz3vdEmgVd3FyrQfaWSXo9uRvc7Fbp5KSLKwMAANd';
 
 const EXPAND_CONTRACTS: any = {};
 [TOKEN_CONTRACT].map((i) => {
