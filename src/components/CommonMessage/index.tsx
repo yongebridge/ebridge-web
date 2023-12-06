d5b8c91f89f97c05ad099e0edc1679db2b1dffef
peter.zhang <hercules@ebridge.exchange>
2023-12-06 19:31:13 +0800

    feat: add error reg

--- src/components/CommonMessage/index.tsx
+++ src/components/CommonMessage/index.tsx
@@ -2,7 +2,7 @@ import { message } from 'antd';
 
 class CommonMessage {
   error(text: any) {
-    if (/You closed the prompt with out any action/.test(text)) {
+    if (/((You closed the prompt with out any action)|(Operation canceled))/.test(text)) {
       return message.error('Request rejected. eBridge needs your permission to continue.');
     }
     message.error(text);
