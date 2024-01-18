6e5df75557504abb23bed44a7705d47134012e74
“Faizal” <mason.stonebear@ebridge.exchange>
2024-01-18 18:53:08 +0800

    feat: feat/EB-27323251 Tron Shasta testnet update

---
+++ src/constants/test1/tron_shasta_testnet.ts
@@ -0,0 +1,37 @@
+export const CHAIN_INFO = {
+  chainId: 2494104990,
+  exploreUrl: 'https://shasta.tronscan.org/',
+  rpcUrl: 'https://api.shasta.trongrid.io',
+  chainName: 'TRON Shasta Testnet',
+  nativeCurrency: {
+    name: 'TRON Shasta Testnet Native Token',
+    symbol: 'sTRX',
+    decimals: 6,
+  },
+  iconUrls: ['https://etherscan.io/token/images/trontrx_32.png'],
+  rpcUrls: ['https://api.shasta.trongrid.io'],
+  blockExplorerUrls: ['https://shasta.tronscan.org/'],
+};
+
+export const BRIDGE_CONTRACT = '0xD032D743A87586039056E3d35894D9F0560E26Be'; // TODO
+export const BRIDGE_CONTRACT_OUT = '0x4C6720dec7C7dcdE1c7B5E9dd2b327370AC9F834'; // TODO
+export const LIMIT_CONTRACT = '0x37cf44B567bA9e2a26E38B777Cc1001b7289324B'; // TODO
+
+/* export const CHAIN_INFO = {
+  chainId: 2494104990,
+  exploreUrl: 'https://shasta.tronscan.org/',
+  rpcUrl: 'http://192.168.67.173:9090',
+  chainName: 'TRON Test1 Testnet',
+  nativeCurrency: {
+    name: 'TRON Test1 Testnet Native Token',
+    symbol: 'sTRX',
+    decimals: 6,
+  },
+  iconUrls: ['https://etherscan.io/token/images/trontrx_32.png'],
+  rpcUrls: ['http://192.168.67.173:9090'],
+  blockExplorerUrls: ['https://shasta.tronscan.org/'],
+};
+
+export const BRIDGE_CONTRACT = '41d0e25d08dc6f390435ce2c4b5c325e3d030d10a2'; // TODO
+export const BRIDGE_CONTRACT_OUT = '41f19186c3a84817886bcc69269dc701cab8804786'; // TODO
+export const LIMIT_CONTRACT = '41da3973e85df4c66c482f618492631d3a494f08fc'; // TODO */
