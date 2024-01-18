5567d292a400cd2acdf9b532bcf5768b8423cf6d
“Faizal” <mason.stonebear@ebridge.exchange>
2024-01-18 15:14:18 +0800

    feat: eb-27323251 - Contract address update

--- src/constants/test1/tron_nile_testnet.ts
+++ src/constants/test1/tron_nile_testnet.ts
@@ -1,4 +1,4 @@
-export const CHAIN_INFO = {
+/* export const CHAIN_INFO = {
   chainId: 3448148188,
   exploreUrl: 'https://nile.tronscan.org/',
   rpcUrl: 'https://api.nileex.io',
@@ -11,8 +11,28 @@ export const CHAIN_INFO = {
   iconUrls: ['https://etherscan.io/token/images/trontrx_32.png'],
   rpcUrls: ['https://api.nileex.io'],
   blockExplorerUrls: ['https://nile.tronscan.org/'],
-};
-
+}; 
 export const BRIDGE_CONTRACT = '0xD032D743A87586039056E3d35894D9F0560E26Be'; // TODO
 export const BRIDGE_CONTRACT_OUT = '0x4C6720dec7C7dcdE1c7B5E9dd2b327370AC9F834'; // TODO
 export const LIMIT_CONTRACT = '0x37cf44B567bA9e2a26E38B777Cc1001b7289324B'; // TODO
+
+*/
+
+export const CHAIN_INFO = {
+  chainId: 3448148188,
+  exploreUrl: 'https://nile.tronscan.org/',
+  rpcUrl: 'http://192.168.67.173:9090',
+  chainName: 'TRON Nile Testnet',
+  nativeCurrency: {
+    name: 'TRON Nile Testnet Native Token',
+    symbol: 'nTRX',
+    decimals: 8,
+  },
+  iconUrls: ['https://etherscan.io/token/images/trontrx_32.png'],
+  rpcUrls: ['http://192.168.67.173:9090'],
+  blockExplorerUrls: ['https://nile.tronscan.org/'],
+};
+
+export const BRIDGE_CONTRACT = '41d0e25d08dc6f390435ce2c4b5c325e3d030d10a2'; // TODO
+export const BRIDGE_CONTRACT_OUT = '41f19186c3a84817886bcc69269dc701cab8804786'; // TODO
+export const LIMIT_CONTRACT = '414765abda9ad4ccd38a61b18fff1983445f3f88fe'; // TODO
