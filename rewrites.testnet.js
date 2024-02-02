269e91e5c853bf60feb594ff53f5fa34823a7329
“Faizal” <mason.stonebear@ebridge.exchange>
2024-02-02 17:39:53 +0800

    feat: eb-3004550541 Update of indexer

--- rewrites.testnet.js
+++ rewrites.testnet.js
@@ -2,5 +2,8 @@ const host = 'https://test.ebridge.exchange';
 module.exports = [
   { source: '/api/:path*', destination: `${host}/api/:path*` },
   { source: '/cms/:path*', destination: `https://test-cms.ebridge.exchange/:path*` },
-  { source: '/AElfIndexer_eBridge/:path*', destination: `http://172.31.11.5:7022/AElfIndexer_eBridge/:path*` },
+  {
+    source: '/AElfIndexer_eBridge/:path*',
+    destination: `https://test-indexer.ebridge.exchange/AElfIndexer_eBridge/:path*`,
+  },
 ];
