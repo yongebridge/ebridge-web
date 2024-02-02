28baf6667799f4eab79d071e5f8834ccd9c9e21d
“Faizal” <mason.stonebear@ebridge.exchange>
2024-02-02 14:59:37 +0800

    feat: eb-3004550541 configuration updated

--- rewrites.test1.js
+++ rewrites.test1.js
@@ -1,12 +1,6 @@
-/* const host = 'http://192.168.66.78:7030';
+const host = 'http://192.168.66.78:7030';
 module.exports = [
   { source: '/api/:path*', destination: `${host}/api/:path*` },
   { source: '/cms/:path*', destination: `https://cms.ebridge.exchange/:path*` },
   { source: '/AElfIndexer_eBridge/:path*', destination: `http://192.168.66.78:7022/AElfIndexer_eBridge/:path*` },
-];*/
-const host = 'https://test.ebridge.exchange';
-module.exports = [
-  { source: '/api/:path*', destination: `${host}/api/:path*` },
-  { source: '/cms/:path*', destination: `https://test-cms.ebridge.exchange/:path*` },
-  { source: '/AElfIndexer_eBridge/:path*', destination: `http://172.31.11.5:7022/AElfIndexer_eBridge/:path*` },
 ];
