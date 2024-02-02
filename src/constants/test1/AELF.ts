b65b757764eea83f4a016f39a97e50e6e48eddb0
“Faizal” <mason.stonebear@ebridge.exchange>
2024-02-02 17:30:06 +0800

    feat: eb-3004550541 configuration updated

--- src/constants/test1/AELF.ts
+++ src/constants/test1/AELF.ts
@@ -1,14 +1,12 @@
 export const CHAIN_INFO = {
   chainId: 'AELF',
   exploreUrl: 'https://explorer-test.aelf.io/',
-  rpcUrl: 'https://aelf-test-node.aelf.io',
-  // rpcUrl: 'http://192.168.66.61:8000',
+  rpcUrl: 'http://192.168.66.61:8000',
 };
 
 export const TOKEN_CONTRACT = 'JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE';
 export const CROSS_CHAIN_CONTRACT = '2SQ9LeGZYSWmfJcYuQkDQxgd3HzwjamAaaL4Tge2eFSXw2cseq';
-export const BRIDGE_CONTRACT = 'foDLAM2Up3xLjg43SvCy5Ed6zaY5CKG8uczj6yUVZUweqQUmz';
-// export const BRIDGE_CONTRACT = '2dKF3svqDXrYtA5mYwKfADiHajo37mLZHPHVVuGbEDoD9jSgE8';
+export const BRIDGE_CONTRACT = '2dKF3svqDXrYtA5mYwKfADiHajo37mLZHPHVVuGbEDoD9jSgE8';
 
 const EXPAND_CONTRACTS: any = {};
 [TOKEN_CONTRACT].map((i) => {
