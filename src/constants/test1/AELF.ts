87f1c0065ccc93da7a0c521920e223f14ebf92ce
faizalebridge <150334207+faizalebridge@users.noreply.github.com>
2024-02-16 11:44:34 +0800

    Update AELF.ts

--- src/constants/test1/AELF.ts
+++ src/constants/test1/AELF.ts
@@ -2,12 +2,11 @@ export const CHAIN_INFO = {
   chainId: 'AELF',
   exploreUrl: 'https://explorer-test.aelf.io/',
   rpcUrl: 'http://192.168.66.61:8000',
-  // rpcUrl: 'https://aelf-test-node.aelf.io',
 };
+
 export const TOKEN_CONTRACT = 'JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE';
 export const CROSS_CHAIN_CONTRACT = '2SQ9LeGZYSWmfJcYuQkDQxgd3HzwjamAaaL4Tge2eFSXw2cseq';
 export const BRIDGE_CONTRACT = '2dKF3svqDXrYtA5mYwKfADiHajo37mLZHPHVVuGbEDoD9jSgE8';
-// export const BRIDGE_CONTRACT = 'foDLAM2Up3xLjg43SvCy5Ed6zaY5CKG8uczj6yUVZUweqQUmz';
 
 const EXPAND_CONTRACTS: any = {};
 [TOKEN_CONTRACT].map((i) => {
